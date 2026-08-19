234999cf37d0a5620a78a184251531dec3a4bee1
juandivg26 <juan.valderrama-g@mail.escuelaing.edu.com>
2026-08-18 22:34:20 -0500

    docs: completa evidencia del ejercicio 1

--- src/main/dosw/semana_1/streams/Ejercicio1.java
+++ src/main/dosw/semana_1/streams/Ejercicio1.java
@@ -1,5 +1,16 @@
-package main.dosw.semana_1.streams;
+package dosw.semana_1.streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Ejercicio1 {
-    
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of(3, 8, 10, 12, 15, 18, 20);
+
+        List<Integer> pares = numeros.stream()
+                .filter(n -> n % 2 == 0 && n > 10)
+                .collect(Collectors.toList());
+
+        System.out.println(pares);
+    }
 }
